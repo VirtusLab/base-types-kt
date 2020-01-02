cbdefedc3db02252d351500abb51bd3d931d8a60
krzykrucz <krzysiuk.66@gmail.com>
2020-01-02 15:09:37 +0100

    removed exceptions from ROP libs

--- result-rxjava/src/main/kotlin/com/virtuslab/basetypes/result/rxjava/SingleResult.kt
+++ result-rxjava/src/main/kotlin/com/virtuslab/basetypes/result/rxjava/SingleResult.kt
@@ -14,22 +14,22 @@ import io.reactivex.Single
 typealias SingleResult<T, E> = Single<Result<T, E>>
 
 
-fun <S : Any, E : Exception, S2 : Any> SingleResult<S, E>.mapSuccess(mapper: (S) -> S2): SingleResult<S2, E> =
+fun <S : Any, E : Any, S2 : Any> SingleResult<S, E>.mapSuccess(mapper: (S) -> S2): SingleResult<S2, E> =
     this.map {
         it.map(mapper)
     }
 
-fun <S : Any, E : Exception, E2 : java.lang.Exception> SingleResult<S, E>.mapFailure(mapper: (E) -> E2): SingleResult<S, E2> =
+fun <S : Any, E : Any, E2 : Any> SingleResult<S, E>.mapFailure(mapper: (E) -> E2): SingleResult<S, E2> =
     this.map {
         it.mapError(mapper)
     }
 
-fun <S : Any, E : Exception, S2 : Any> SingleResult<S, E>.flatMapResult(mapper: (S) -> Result<S2, E>): SingleResult<S2, E> =
+fun <S : Any, E : Any, S2 : Any> SingleResult<S, E>.flatMapResult(mapper: (S) -> Result<S2, E>): SingleResult<S2, E> =
     this.map {
         it.flatMap(mapper)
     }
 
-fun <S : Any, E : Exception, S2 : Any> SingleResult<S, E>.flatMapSuccess(mapper: (S) -> SingleResult<S2, E>): SingleResult<S2, E> =
+fun <S : Any, E : Any, S2 : Any> SingleResult<S, E>.flatMapSuccess(mapper: (S) -> SingleResult<S2, E>): SingleResult<S2, E> =
     this.flatMap { result1 ->
         when (result1) {
             is Success -> mapper(result1.value)
@@ -37,12 +37,12 @@ fun <S : Any, E : Exception, S2 : Any> SingleResult<S, E>.flatMapSuccess(mapper:
         }
     }
 
-//fun <S : Any, E : Exception, S2 : Any, E2 : Exception> SingleResult<S, E>.flatMap(mapper: (S) -> SingleResult<S2, E2>, errorMapper: (E) -> E2): SingleResult<S2, E2> = TODO()
+//fun <S : Any, E : Any, S2 : Any, E2 : Any> SingleResult<S, E>.flatMap(mapper: (S) -> SingleResult<S2, E2>, errorMapper: (E) -> E2): SingleResult<S2, E2> = TODO()
 //
-//fun <S : Any, E : Exception, S2 : Any, E2 : Exception> SingleResult<S, E>.flatMapResult(mapper: (S) -> Result<S2, E2>, errorMapper: (E) -> E2): SingleResult<S2, E2> = TODO()
+//fun <S : Any, E : Any, S2 : Any, E2 : Any> SingleResult<S, E>.flatMapResult(mapper: (S) -> Result<S2, E2>, errorMapper: (E) -> E2): SingleResult<S2, E2> = TODO()
 
 
-fun <S : Any, E : Exception, S2 : Any> SingleResult<S, E>.flatMapSuccess(mapper: (S) -> Single<S2>, errorMapper: (Throwable) -> E): SingleResult<S2, E> =
+fun <S : Any, E : Any, S2 : Any> SingleResult<S, E>.flatMapSuccess(mapper: (S) -> Single<S2>, errorMapper: (Throwable) -> E): SingleResult<S2, E> =
     this.flatMap { result1 ->
         when (result1) {
             is Success -> mapper(result1.value).liftResult(errorMapper)
@@ -50,38 +50,38 @@ fun <S : Any, E : Exception, S2 : Any> SingleResult<S, E>.flatMapSuccess(mapper:
         }
     }
 
-fun <S : Any, E : Exception, S2 : Any> Result<S, E>.liftMap(mapper: (S) -> SingleResult<S2, E>): SingleResult<S2, E> =
+fun <S : Any, E : Any, S2 : Any> Result<S, E>.liftMap(mapper: (S) -> SingleResult<S2, E>): SingleResult<S2, E> =
     when (this) {
         is Success -> mapper(value)
         is Failure -> Failure(error).toSingle()
     }
 
-fun <S : Any, E : Exception> S.justSingleResult(): SingleResult<S, E> =
+fun <S : Any, E : Any> S.justSingleResult(): SingleResult<S, E> =
     Result.of<S, E> { this }
         .let { Single.just(it) }
 
 
-fun <S : Any, E : Exception> E.failedSingleResult(): SingleResult<S, E> =
+fun <S : Any, E : Any> E.failedSingleResult(): SingleResult<S, E> =
     Result.error(this)
         .let { Single.just(it) }
 
-fun <S : Any, E : Exception> Result<S, E>.liftSingle(): SingleResult<S, E> =
+fun <S : Any, E : Any> Result<S, E>.liftSingle(): SingleResult<S, E> =
     this.toSingle()
 
-fun <S : Any, E : Exception> Single<S>.liftResult(errorMapper: (Throwable) -> E): SingleResult<S, E> =
+fun <S : Any, E : Any> Single<S>.liftResult(errorMapper: (Throwable) -> E): SingleResult<S, E> =
     this.map { Result.success(it) as Result<S, E> }
         .onErrorReturn { Result.error(errorMapper(it)) }
 
 fun <S> S.toSingle(): Single<S> = Single.just(this)
 
 // TODO test
-fun <S : Any, E : Exception> Result<S, E>.toFailure(): Option<E> = this.fold({ None }, { Some(it) })
+fun <S : Any, E : Any> Result<S, E>.toFailure(): Option<E> = this.fold({ None }, { Some(it) })
 
 // TODO test
-fun <S : Any, E : Exception> SingleResult<S, E>.toFailure(): Single<Option<E>> = this.map { it.toFailure() }
+fun <S : Any, E : Any> SingleResult<S, E>.toFailure(): Single<Option<E>> = this.map { it.toFailure() }
 
 // TODO test
-fun <S : Any, E : Exception> Result<S, E>.toSuccess(): Option<S> = this.fold({ Some(it) }, { None })
+fun <S : Any, E : Any> Result<S, E>.toSuccess(): Option<S> = this.fold({ Some(it) }, { None })
 
 // TODO test
-fun <S : Any, E : Exception> SingleResult<S, E>.toSuccess(): Single<Option<S>> = this.map { it.toSuccess() }
+fun <S : Any, E : Any> SingleResult<S, E>.toSuccess(): Single<Option<S>> = this.map { it.toSuccess() }
