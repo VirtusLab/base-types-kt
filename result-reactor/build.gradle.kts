2b8836f9e90faf691fe53e5be97a5a083fe27bdc
krzykrucz <krzysiuk.66@gmail.com>
2020-02-11 09:42:41 +0100

    reactor upgrade

--- result-reactor/build.gradle.kts
+++ result-reactor/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     val junit: String by project
     val kotlinVersion: String by project
     val kotlintest: String by project
-    val reactor = "3.2.12.RELEASE"
+    val reactor = "3.3.0.RELEASE"
 
     implementation(kotlin("stdlib", kotlinVersion))
 
