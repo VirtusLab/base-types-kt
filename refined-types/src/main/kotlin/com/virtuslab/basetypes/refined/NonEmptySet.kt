d0a7b3d6edb0cff4b321d1a73967aad1ab7e2593
krzykrucz <krzysiuk.66@gmail.com>
2020-03-09 16:44:57 +0100

    NonEmptySet fix

--- refined-types/src/main/kotlin/com/virtuslab/basetypes/refined/NonEmptySet.kt
+++ refined-types/src/main/kotlin/com/virtuslab/basetypes/refined/NonEmptySet.kt
@@ -1,12 +1,14 @@
 package com.virtuslab.basetypes.refined
 
+import arrow.core.Option
+import arrow.core.maybe
 
-class NonEmptySet<T> private constructor(private val elements: Set<T>) : Set<T> by elements {
+class NonEmptySet<T> private constructor(private val elements: Set<T>) : Set<T> by elements { // TODO do not implement Set, methods should return NonEmptySet
     companion object {
         fun <T> create(element: T): NonEmptySet<T> = NonEmptySet(setOf(element))
 
         fun <T> create(first: T, vararg rest: T): NonEmptySet<T> = NonEmptySet(setOf(first) + rest)
 
-        fun <T> create(set: Set<T>): NonEmptySet<T>? = NonEmptySet(set)
+        fun <T> create(set: Set<T>): Option<NonEmptySet<T>> = set.isNotEmpty().maybe { NonEmptySet(set) }
     }
 }
