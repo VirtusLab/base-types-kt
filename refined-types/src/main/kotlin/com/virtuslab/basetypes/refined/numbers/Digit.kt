06a02d329d7bf93cb77bcae0f845af8aa746e46d
krzykrucz <krzysiuk.66@gmail.com>
2020-03-09 16:40:04 +0100

    better Digit

--- refined-types/src/main/kotlin/com/virtuslab/basetypes/refined/numbers/Digit.kt
+++ refined-types/src/main/kotlin/com/virtuslab/basetypes/refined/numbers/Digit.kt
@@ -1,28 +1,40 @@
 package com.virtuslab.basetypes.refined.numbers
 
 import arrow.core.Option
-import arrow.core.toOption
+import arrow.core.some
 
-enum class Digit(val digit: Int) {
+sealed class Digit(val digit: Int) {
 
-    Zero(0),
-    One(1),
-    Two(2),
-    Three(3),
-    Four(4),
-    Five(5),
-    Six(6),
-    Seven(7),
-    Eight(8),
-    Nine(9);
+    companion object {
+        fun of(int: Int): Option<Digit> =
+            when (int) {
+                0 -> Zero.some()
+                1 -> One.some()
+                2 -> Two.some()
+                3 -> Three.some()
+                4 -> Four.some()
+                5 -> Five.some()
+                6 -> Six.some()
+                7 -> Seven.some()
+                8 -> Eight.some()
+                9 -> Nine.some()
+                else -> Option.empty()
+            }
 
-    fun of(int: Int): Option<Digit> =
-        values()
-            .find { it.digit == int }
-            .toOption()
-
-    fun of(int: String): Option<Digit> =
-        int.toIntOrNull()
-            .let { Option.fromNullable(it) }
-            .flatMap(::of)
+        fun of(int: String): Option<Digit> =
+            int.toIntOrNull()
+                .let { Option.fromNullable(it) }
+                .flatMap(::of)
+    }
 }
+
+object Zero : Digit(0)
+object One : Digit(1)
+object Two : Digit(2)
+object Three : Digit(3)
+object Four : Digit(4)
+object Five : Digit(5)
+object Six : Digit(6)
+object Seven : Digit(7)
+object Eight : Digit(8)
+object Nine : Digit(9)
